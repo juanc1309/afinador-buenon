1b1a6f6ba40bc63fb34427ff628ff0aa2f1d79ca
ruloph <rulop008@icloud.com>
2026-02-05 22:58:36 -0600

    modified:   script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 // Script para integrar un modelo de Teachable Machine (audio) y actualizar la UI
 // 1) Pon tu URL de modelo de AUDIO en MODEL_URL (carpeta que contiene model.json)
 // 2) Abre la página en un navegador y acepta acceso al micrófono
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Test TM Audio</title>
-</head>
-<body>
 
-<button id="btn">Activar micrófono</button>
-<p id="out">Esperando...</p>
-
-<script src="https://cdn.jsdelivr.net/npm/@teachablemachine/audio@0.8/dist/teachablemachine-audio.min.js"></script>
-<script>
 const URL = "./teach/";
 let model, listening = false;
 
@@ -34,9 +22,5 @@ async function start() {
 
 document.getElementById("btn").onclick = start;
 loadModel();
-</script>
-
-</body>
-</html>
 
 
